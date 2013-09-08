21f56785d9d830c7aa16198a7cb48ae671d904c2
majora2007 <josephmajora@gmail.com>
2013-09-07 18:42:01 -0700

    Implemented logic behind storing and withdrawing xp from bank. Currently, if there is an overflow (too much xp withdrawing), nothing happens (as I haven't worked out the logic yet).

---
+++ src/com/majora/minecraft/experienceshelves/listeners/PlayerListener.java
@@ -0,0 +1,191 @@
+package com.majora.minecraft.experienceshelves.listeners;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import com.majora.minecraft.experienceshelves.ExperienceShelves;
+import com.majora.minecraft.experienceshelves.models.XPVault;
+
+public class PlayerListener implements Listener {
+	
+	private ExperienceShelves plugin;
+	
+	private Map<Location, XPVault> vaults;
+	
+	// NOTE: It looks like xp is store internally as a char, meaning max xp is 
+	// 65,635
+	final int MAX_EXP = 65635;
+	
+	
+	public PlayerListener(ExperienceShelves instance) {
+		this.plugin = instance;
+		
+		vaults = new HashMap<Location, XPVault>();
+	}
+	
+	@EventHandler
+	public void onPlayerClick(PlayerInteractEvent event)
+	{
+		if(!event.hasBlock()) return;
+		
+		if (isRightClick( event ))
+		{
+			if (isClickedBlockXPVault( event ))
+			{
+				final Player player = event.getPlayer();
+				
+				final int totalXp = calcTotalXp(player);
+				final Block clickedBlock = event.getClickedBlock();
+				
+				// Let's pull out the essential info from Block
+				final Location blockLoc = clickedBlock.getLocation();
+				
+				XPVault accessedVault = null;
+				if (vaults.containsKey(blockLoc)) {
+					accessedVault = vaults.get(clickedBlock.getLocation());
+					
+					// TODO: Perform extra check here to make sure Block is still a valid vault
+					
+				} else {
+					accessedVault = createXPVault(clickedBlock, player, totalXp);
+					vaults.put(clickedBlock.getLocation(), accessedVault);
+				}
+				
+				assert(accessedVault != null);
+				// At this point, we can now check the mode and whether the vault is locked or not
+				if (accessedVault.isLocked()) {
+					// Tell player they must unlock before they interact
+					player.sendMessage("You must unlock the vault before you can interact.");
+					return;
+				} else {
+					if (accessedVault.getMode() == 0) // Store mode
+					{
+						accessedVault.setBalance(totalXp);
+						// Clear player's xp level
+					} else if (accessedVault.getMode() == 1) // Withdraw mode
+					{
+						// We need to worry about overflow here. If player has xp amt which
+						// is > MAX - vault's xp, an overflow will happen (thus xp loss).
+						// We should keep leftover xp.
+						
+						int leftoverXp = MAX_EXP - (totalXp + accessedVault.getBalance());
+						
+						if ( leftoverXp < 0)
+						{
+							// There is leftover, so let's take abs value and store into vault
+							//player.setLevel(level);
+							//player.setExp(exp);
+							// TODO
+							
+							accessedVault.setBalance(Math.abs(leftoverXp));
+							accessedVault.setMode(0);
+						} else {
+							// There is no leftover, so just add the balance to the user
+							int tempBalance = accessedVault.getBalance();
+							while (tempBalance >= player.getExpToLevel())
+							{
+								int currentLevel = player.getLevel();
+								tempBalance -= player.getExpToLevel();
+								player.setLevel(currentLevel+1);
+							}
+							
+							// At this point tempBalance is less than next level, so we calculate the percentage till next level, 
+							// and set it.
+							float percentage = tempBalance / player.getExpToLevel();
+							player.setExp(percentage);
+							
+							accessedVault.setBalance(0);
+							accessedVault.setMode(1);
+						}
+						
+						player.sendMessage(accessedVault.getBalance() - MAX_EXP + " has been withdraw.");
+						player.sendMessage("You have a leftover balance of: " + accessedVault.getBalance() + " xp.");
+					}
+				}
+				
+				
+			}
+		}
+	}
+	
+	private XPVault createXPVault(Block clickedBlock, Player player, int totalXp) {
+		XPVault vault = new XPVault();
+		vault.setBlockMaterial(clickedBlock.getType());
+		vault.setBlockX(clickedBlock.getX());
+		vault.setBlockY(clickedBlock.getY());
+		vault.setBlockZ(clickedBlock.getZ());
+		vault.setWorldName(clickedBlock.getWorld().getName());
+		vault.setOwnerName(player.getName());
+		//vault.setBalance(totalXp); // We do this only after we confirm mode
+		
+		return vault;
+	}
+
+	//http://www.minecraftwiki.net/wiki/Experience
+	private int calcTotalXp(Player player) {
+		final float expPercent = player.getExp();
+		final int expToNextLevel = player.getExpToLevel();
+		final int currentLevel = player.getLevel();
+		
+		int totalXp = 0;
+		
+		if (currentLevel <= 15)
+		{
+			// There is a staic 17 xp between levels
+			// The reason for times 100 / 100 is to eliminate floating point errors without causing a huge loss of xp. We only care about 2 decimal points.
+			totalXp = currentLevel * 17 + ( (int) (expPercent*100) * expToNextLevel) / 100;
+		} else if (currentLevel <= 30)
+		{
+			float baseLvlXp = ((1.5f * (currentLevel * currentLevel)) - (29.5f * currentLevel) + 360);
+			totalXp = (int) (baseLvlXp + ( (int) (expPercent*1000) * expToNextLevel) / 1000);
+		} else if (currentLevel >= 31)
+		{
+			float baseLvlXp = ((3.5f * (currentLevel * currentLevel)) - (151.5f * currentLevel) + 2220);
+			totalXp = (int) (baseLvlXp + ( (int) (expPercent*1000) * expToNextLevel) / 1000);
+		}
+		
+		return totalXp;
+	}
+	
+	// This does not work!
+	private int calcXpLevel(int totalXp)
+	{
+		int level = 0;
+		
+		final int level15XP = 255;
+		final int level30XP = 825;
+		final int level31XP = 887;
+		
+		if (totalXp <= level15XP)
+		{
+			level = (int) Math.floor(totalXp / 17);
+		} else if (totalXp <= level30XP)
+		{
+			//level = ((1.5f * (currentLevel * currentLevel)) - (29.5f * currentLevel) + 360);
+			//level = ((1.5f * (currentLevel * currentLevel)) - (29.5f * currentLevel) + 360);
+		}
+		
+		
+		return level;
+	}
+
+	private boolean isRightClick( PlayerInteractEvent event )
+	{
+		return (event.getAction() == Action.RIGHT_CLICK_BLOCK);
+	}
+	
+	private boolean isClickedBlockXPVault( PlayerInteractEvent event )
+	{
+		return event.getClickedBlock().getType() == Material.BOOKSHELF;
+	}
+
+}
---
+++ src/com/majora/minecraft/experienceshelves/models/XPVault.java
@@ -0,0 +1,129 @@
+package com.majora.minecraft.experienceshelves.models;
+
+import org.bukkit.Material;
+
+/**
+ * XPVault represents a vault of xp. 
+ * @author Joseph
+ *
+ */
+public class XPVault {
+	
+	/*
+	 * This is what I am thinking about in terms of data storage.
+	 * What I know we need is the location of the bookshelf, the player 
+	 * who 'owns' the shelf, and optional meta-data such as if the shelf is locked.
+	 * 
+	 * The location is tricky as there may be multiple worlds, so we should also store a world 
+	 * identifier.
+	 * 
+	 * We also need to take permissions into account as in order to lock a vault, we must type a command, so 
+	 * we should have permissions.
+	 * 
+	 * Commands: 
+	 * 	lock - user can lock their shelves
+	 * break - user can break ANY shelves
+	 * store - user can store xp in shelves
+	 * withdraw - user can withdraw xp from shelves
+	 * 
+	 * The way MagicBookshelf worked was:
+	 * You right click to toggle between store and withdrawing.
+	 * You can place a sign with [XP_Private] and 3 usernames to lock a bookshelf to.
+	 * 
+	 * I also would like to address moving bookshelves. This can be handled where the user has to withdraw 
+	 * break, move, place, then store. Or, we can store xp in meta-data (with JSON backend) and just fill-in 
+	 * the meta-data at reload/enable. I think for the first, we should stick to keeping it only in JSON.
+	 */
+	
+	private int mode = 0; // TODO: Refactor to Enum for STORE/WITHDRAW
+	
+	private int balance = 0; // Current balance of the vault (xp).
+	
+	// Now we need some information describing Minecraft Block and Player who owns it.
+	private String ownerName = null; // In order to locate a Player on a server, we only need the account name.
+	
+	// For location, I need x, y, z, block type, and name of world
+	private String worldName = null;
+	private int blockX = 0, blockY = 0, blockZ = 0;
+	private Material blockMaterial = null;
+	
+	// Lastly we need to maintain optional variables which manage control
+	private boolean locked = false;
+	
+	public XPVault() {}
+
+	public int getMode() {
+		return mode;
+	}
+
+	public void setMode(int mode) {
+		this.mode = mode;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getOwnerName() {
+		return ownerName;
+	}
+
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
+	}
+
+	public String getWorldName() {
+		return worldName;
+	}
+
+	public void setWorldName(String worldName) {
+		this.worldName = worldName;
+	}
+
+	public int getBlockX() {
+		return blockX;
+	}
+
+	public void setBlockX(int blockX) {
+		this.blockX = blockX;
+	}
+
+	public int getBlockY() {
+		return blockY;
+	}
+
+	public void setBlockY(int blockY) {
+		this.blockY = blockY;
+	}
+
+	public int getBlockZ() {
+		return blockZ;
+	}
+
+	public void setBlockZ(int blockZ) {
+		this.blockZ = blockZ;
+	}
+
+	public Material getBlockMaterial() {
+		return blockMaterial;
+	}
+
+	public void setBlockMaterial(Material blockMaterial) {
+		this.blockMaterial = blockMaterial;
+	}
+
+	public boolean isLocked() {
+		return locked;
+	}
+
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
+	
+	
+
+}
