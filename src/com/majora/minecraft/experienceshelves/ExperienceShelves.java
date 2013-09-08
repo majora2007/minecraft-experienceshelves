6d2ff6c01c45fdbe98d0d69edc079635d55c7ef0
majora2007 <josephmajora@gmail.com>
2013-09-07 17:06:09 -0700

    Implemented a basic plugin which tells the current xp until next level. The XPVault has notes for how the plugin will work.

--- src/com/majora/minecraft/experienceshelves/ExperienceShelves.java
+++ src/com/majora/minecraft/experienceshelves/ExperienceShelves.java
@@ -5,20 +5,40 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.majora.minecraft.experienceshelves.listeners.PlayerListener;
+
 public final class ExperienceShelves extends JavaPlugin {
 	
-	public static Logger consoleLogger = Logger.getLogger("Minecraft");
-	public static String pluginLogPrefix;
+	private static Logger consoleLogger = Logger.getLogger("Minecraft");
+	private static String pluginLogPrefix;
+	
+	private PlayerListener playerListener;
 	
 	@Override
-	public void onEnable() {}
+	public void onEnable() 
+	{
+		initializeLoggerPrefix();
+		
+		this.playerListener = new com.majora.minecraft.experienceshelves.listeners.PlayerListener(this);
+		getServer().getPluginManager().registerEvents(this.playerListener, this);
+		
+		ExperienceShelves.log("ExperienceShelves has been enabled");
+	}
 	
 	@Override
-	public void onDisable() {}
+	public void onDisable() 
+	{
+		ExperienceShelves.log("ExperienceShelves has been disabled");
+	}
 	
-	private void declareLoggerPrefix()
+	private void initializeLoggerPrefix()
 	{
 		PluginDescriptionFile pluginDescriptionFile = getDescription();
-		pluginLogPrefix = "[" +  pluginDescriptionFile.getName() + " version: " + pluginDescriptionFile.getVersion() + "] - ";
+		pluginLogPrefix = "[" +  pluginDescriptionFile.getName() + "]: ";
+	}
+	
+	public static void log(final String msg)
+	{
+		ExperienceShelves.consoleLogger.info(pluginLogPrefix + msg);
 	}
 }
